bc0549ab044c382b9288840ad2a89f193b6a1ad4
Zsolt Kacsandi <zsolt@kacsandi.com>
2025-02-15 12:53:22 +0100

    build: add test, lint, fmt, vet tasks

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,11 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.go-task
+    pkgs.golangci-lint
+  ];
 
   languages.go.enable = true;
 }
